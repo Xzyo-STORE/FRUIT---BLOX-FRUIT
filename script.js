05c2cc46956181496f322ad929baae9d3ef4f35d
Xzyo <rahmat02riyadi@gmail.com>
2026-03-04 01:06:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -219,9 +219,9 @@ function kirimFormSubmit(tid, u, w, itm, tot) {
     const telegramToken = "8411373244:AAG02xoquXi2vWxURfLu5wJHxdJX62h-PMU";
     const telegramChatId = "8262559652";
     
-    const linkKonfirmasi = `https://xzyo-store.vercel.app/admin.html?tid=${tid}`;
+    const linkKonfirmasi = `https://xzyo-store.vercel.app/FRUIT---BLOX-FRUIT/admin.html?tid=${tid}`;
 
-    const pesan = `🚀 *PESANAN BARU - XZYO STORE*%0A` +
+    const pesan = `🚀 *PESANAN FRUIT BARU*%0A` +
                   `━━━━━━━━━━━━━━━━━━━━%0A` +
                   `🆔 *Order ID:* \`${tid}\` %0A` +
                   `👤 *Username:* ${u}%0A` +
@@ -274,6 +274,7 @@ function potongStokOtomatis(itmString) {
 }
 
 window.onload = init;
+
 
 
 
